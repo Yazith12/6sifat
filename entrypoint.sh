b983538b01d5a13fda4e08431a4b7d1d12586c3e
Yazith <yazithaaland9@gmail.com>
2025-09-01 01:10:58 +0800

    Fix entrypoint to keep container running

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 set -e
 
-# First arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-    set -- php-fpm "$@"
-fi
-
 # Run migrations if needed
 if [ "$APP_ENV" = "production" ] && [ "$RUN_MIGRATIONS" = "true" ]; then
     echo "Running migrations..."
     php artisan migrate --force
 fi
 
-# Start Nginx in background
-nginx -g "daemon off;" &
+# Start PHP-FPM in the background
+php-fpm &
 
-# Start PHP-FPM
-if [ "$1" = 'php-fpm' ]; then
-    exec "$@"
-fi
-
-exec "$@"
+# Start Nginx as the foreground process (keeps container running)
+exec nginx -g "daemon off;"
